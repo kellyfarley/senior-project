bc1e1ce05ca7ba15668d08307144fd12854cd8cb
Kelly Farley <farleykc@gmail.com>
2018-05-08 15:12:05 -0500

    first comit

---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+
+get '/input' do
+	erb :input
+end
+
+=begin
+get '/input/result' do
+	@text = "Hello. My name is Kelly. I really hope my code works."
+	#replace text with user input
+	textarray = @text.split
+	@wordcount = textarray.length
+	sentencearray = @text.split(".")
+	@sentencecount = sentencearray.length
+	erb :result
+end
+=end
+
+get '/input/result' do
+	erb :result
+end
+
+post '/result' do
+end
---
+++ public/scripts/logic.js
@@ -0,0 +1,40 @@
+// find stuff
+/*
+"How are you?".indexOf("o");
+"How are you?".charAt(5);
+
+// replace stuff
+const newString = originalString.replace(/javascript/gi, "JavaScript");
+
+function replace(){
+	text = $(@text).html().replace(/e/g, '<span class="red">e</span>');
+}
+
+// highlight stuff
+for (var i = 0; i < str.length; i++) {
+     if (str[i] == 'e') {
+     newText+= str.charAt(i).fontcolor("red");
+     }
+     else {
+        newText += str[i];
+    }
+}
+s1.innerHTML = newText;
+
+function color_word(text_id, word, color) {
+    words = $('#' + text_id).text().split(' ');
+    words = words.map(function(item) { return item == word ? "<span style='color: " + color + "'>" + word + '</span>' : item });
+    new_words = words.join(' ');
+    $('#' + text_id).html(new_words);
+    }
+
+// puts the entire thing in a span tag so that's an issue
+function checkInput(){
+	var input = $('textarea').val();
+	if(input=="hello"){
+		$('p').append('<span>'+input+'</span>')
+	}
+};
+*/
+
+// maybe look into set interval
