acc83dfd3eb6427c6d19613407a1c0d60149f372
Kelly Farley <farleykc@gmail.com>
2018-05-16 14:01:01 -0500

    can deal with input with punctuation

--- public/scripts/logic.js
+++ public/scripts/logic.js
@@ -41,7 +41,7 @@ function checkInput(){
 
 // take the input
 //var text = <%@userInput%>
-var text = "you'll be back soon you'll see it's all page however page good I swear isn't it lovely very lovely"
+var text = "you'll be back get soon you'll see; it's all; page. however page get. good I swear isn't it lovely very lovely"
 
 // word count
 var textArray = text.split(" ");
@@ -71,31 +71,39 @@ console.log(avgSentence);
 
 // highlight bad punctuation, exclamation marks and question marks (FIX LATER)
 
+// text array but without the puncuation or caps
+var cleanText = text.toLowerCase();
+var cleanText = cleanText.replace(/\./g, '');
+var cleanText = cleanText.replace(/\,/g, '');
+var cleanText = cleanText.replace(/\;/g, '');
+var cleanTextArray = cleanText.split(" ")
+console.log(cleanTextArray);
+
 // weak verbs
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
   var weakVerbs = ["get", "gets", "say", "says", "proves", "prove", "seem", "seems", "begin", "begins"];
   for (x=0; x<weakVerbs.length; x++) {
-  	 if (textArray[i] == weakVerbs[x]) {
+  	 if (cleanTextArray[i] == weakVerbs[x]) {
     	textArray[i]= '<span class="red">'+textArray[i]+'</span>';
   	}
   }
 }
 
 // vague nouns
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
 	var weakNouns = ["stuff", "thing", "things", "everyone", "everybody"];
 	for (x=0; x<weakNouns.length; x++) {
-  	 if (textArray[i] == weakNouns[x]) {
+  	 if (cleanTextArray[i] == weakNouns[x]) {
     	textArray[i]= '<span class="purple">'+textArray[i]+'</span>';
   	}
   }
 }
 
-// overused words (WHY IS THIS BEING PURPLE)
-for (i=0; i<textArray.length; i++) {
+// overused words
+for (i=0; i<cleanTextArray.length; i++) {
 	var weakWords = ["good", "like", "just", "evilness", "there", "large", "like"];
 	for (x=0; x<weakWords.length; x++) {
-  	 if (textArray[i] == weakWords[x]) {
+  	 if (cleanTextArray[i] == weakWords[x]) {
     	textArray[i]= '<span class="green">'+textArray[i]+'</span>';
   	}
   }
@@ -103,65 +111,65 @@ for (i=0; i<textArray.length; i++) {
 // replace "like" with "such as"?
 
 // passive voice
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
 	var passiveVoice = ["is", "am", "are"];
 	for (x=0; x<passiveVoice.length; x++) {
-  	 if (textArray[i] == passiveVoice[x]) {
+  	 if (cleanTextArray[i] == passiveVoice[x]) {
     	textArray[i]= '<span class="orange">'+textArray[i]+'</span>';
   	}
   }
 }
 
-// naked this
-for (i=0; i<textArray.length; i++) {
-	if (textArray[i] == "this") {
+// naked this - maybe only highlight if follwed by "shows"?
+for (i=0; i<cleanTextArray.length; i++) {
+	if (cleanTextArray[i] == "this") {
     	textArray[i]= '<span class="yellow">'+textArray[i]+'</span>';
   	}
 }
 
 // 2nd person
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
 	var secondPerson = ["you", "you're", "you'll", "you'd"];
 	for (x=0; x<secondPerson.length; x++) {
-  	 if (textArray[i] == secondPerson[x]) {
+  	 if (cleanTextArray[i] == secondPerson[x]) {
     	textArray[i]= '<span class="orange">'+textArray[i]+'</span>';
   	}
   }
 }
 
 // first person
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
 	var firstPerson = ["I", "me", "we", "our", "I'm", "I'll", "I'd", "we're", "we'd", "we'll"];
 	for (x=0; x<firstPerson.length; x++) {
-  	 if (textArray[i] == firstPerson[x]) {
+  	 if (cleanTextArray[i] == firstPerson[x]) {
     	textArray[i]= '<span class="orange">'+textArray[i]+'</span>';
   	}
   }
 }
 
 // adverbs
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
 	var adverbs = ["carefully", "very", "happily", "quickly", "really", "finally", "seriously", "always", "badly", "exactly", "basically", "actually", "literally", "clearly", "totally", "obviously", "truly", "constantly","specifically"];
 	for (x=0; x<adverbs.length; x++) {
-  	 if (textArray[i] == adverbs[x]) {
+  	 if (cleanTextArray[i] == adverbs[x]) {
     	textArray[i]= '<span class="orange">'+textArray[i]+'</span>';
   	}
   }
 }
 
 // past tense - check for words ending in ED? not applicable for history
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
 	var pastTense = ["said", "made", "went", "took", "came", "saw", "knew", "got", "gave", "found", "thought", "told", "became", "showed", "left", "put", "brought", "began", "kept", "held", "wrote", "stood", "heard", "let", "meant", "set", "met", "run", "paid", "said", "understood", "bought", "worn", "died", "was", "were", "did"];
 	for (x=0; x<adverbs.length; x++) {
-  	 if (textArray[i] == adverbs[x]) {
+  	 if (cleanTextArray[i] == adverbs[x]) {
     	textArray[i]= '<span class="orange">'+textArray[i]+'</span>';
   	}
   }
 }
 
 // page
-for (i=0; i<textArray.length; i++) {
-	if (textArray[i] == "page") {
+for (i=0; i<cleanTextArray.length; i++) {
+	if (cleanTextArray[i] == "page") {
 		textArray[i]= '<span class="orange">'+textArray[i]+'</span>';
 	}
 }
@@ -175,10 +183,10 @@ for (i=0; i<textArray.length; i++) {
 } */
 
 // transitions
-for (i=0; i<textArray.length; i++) {
+for (i=0; i<cleanTextArray.length; i++) {
 	var transitions = ["conclusion", "next", "however", "additionally", "moreover", "therefore", "overall", "even"];
 	for (x=0; x<transitions.length; x++) {
-  	 if (textArray[i] == transitions[x]) {
+  	 if (cleanTextArray[i] == transitions[x]) {
     	textArray[i]= '<span class="orange">'+textArray[i]+'</span>';
   	}
   }
@@ -189,6 +197,8 @@ for (i=0; i<textArray.length; i++) {
 // weak quote intro
 
 // bad phrases
+
+// this shows?
 
 // FIND AND REPLACE
 
@@ -205,3 +215,17 @@ $(".orange").click(function() {
 });
 
 // repetition (FIX LATER)
+
+// remove last stuff
+function removeLastChar(str) {
+  if (str.slice(-1) == ';') {
+    var str = str.slice(0, -1);  
+  }
+  if (str.slice(-1) == ',') {
+    var str = str.slice(0, -1);  
+  }
+  if (str.slice(-1) == '.') {
+    var str = str.slice(0, -1);  
+  }
+  //console.log(str);
+}
