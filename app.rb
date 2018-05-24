25e900c3d33e43e7c2df961553796f72ae0caec0
Kelly Farley <farleykc@gmail.com>
2018-05-24 10:55:28 -0500

    testing

--- app.rb
+++ app.rb
@@ -4,20 +4,20 @@ get '/' do
 	erb :input
 end
 
-# get '/result' do
-# 	@userText = params[:userInput]
-# 	#word count
-# 	@userText = @userText.to_s
-# 	@userTextArray = @userText.split(" ")
-# 	@wordCount = @userTextArray.length
-# 	#sentence count
-# 	@sentenceArray = @userText.split(".")
-# 	@sentenceCount = @sentenceArray.length
-# 	#average word length
-# 	@characterCount = @userText.length
-# 	@letterCount = @characterCount - @wordCount + 1
-# 	@avgWord = @letterCount / @wordCount
-# 	#average sentence length
-# 	@avgSentence = @wordCount / @sentenceCount
-# 	erb :result
-# end
+get '/result' do
+	@userText = params[:userInput]
+	#word count
+	@userText = @userText.to_s
+	@userTextArray = @userText.split(" ")
+	@wordCount = @userTextArray.length
+	#sentence count
+	@sentenceArray = @userText.split(".")
+	@sentenceCount = @sentenceArray.length
+	#average word length
+	@characterCount = @userText.length
+	@letterCount = @characterCount - @wordCount + 1
+	@avgWord = @letterCount / @wordCount
+	#average sentence length
+	@avgSentence = @wordCount / @sentenceCount
+	erb :result
+end
