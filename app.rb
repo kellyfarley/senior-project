f9dc225ff44ee626b9e550045bdf3d7998e11736
Kelly Farley <farleykc@gmail.com>
2018-05-28 11:35:14 -0500

    params user input is working again

--- app.rb
+++ app.rb
@@ -7,17 +7,17 @@ end
 post '/result' do
 	@userText = params[:userInput]
 	#word count
-	#@userText = @userText.to_s
-	#@userTextArray = @userText.split(" ")
-	#@wordCount = @userTextArray.length
+	@userText = @userText.to_s
+	@userTextArray = @userText.split(" ")
+	@wordCount = @userTextArray.length
 	#sentence count
-	#@sentenceArray = @userText.split(".")
-	#@sentenceCount = @sentenceArray.length
+	@sentenceArray = @userText.split(".")
+	@sentenceCount = @sentenceArray.length
 	#average word length
-	#@characterCount = @userText.length
-	#@letterCount = @characterCount - @wordCount + 1
-	#@avgWord = @letterCount / @wordCount
+	@characterCount = @userText.length
+	@letterCount = @characterCount - @wordCount + 1
+	@avgWord = @letterCount / @wordCount
 	#average sentence length
-	#@avgSentence = @wordCount / @sentenceCount
+	@avgSentence = @wordCount / @sentenceCount
 	erb :result
 end
